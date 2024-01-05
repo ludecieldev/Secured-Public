01dcd3103847a625c818978d4f22b07fb89f68cf
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-01-05 20:07:31 +0100

    [+] Added include.h

---
+++ include/include.h
@@ -0,0 +1,58 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPE-110-LIL-1-1-secured-alexandre.garbe
+** File description:
+** include.h
+** Author:
+** ludeciel
+*/
+
+#ifndef B_CPE_110_LIL_1_1_SECURED_ALEXANDRE_GARBE_INCLUDE_H
+    #define B_CPE_110_LIL_1_1_SECURED_ALEXANDRE_GARBE_INCLUDE_H
+    #include <stdlib.h>
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <string.h>
+    #include <stdarg.h>
+    #include <stdbool.h>
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <fcntl.h>
+    #include <errno.h>
+    #include <time.h>
+    #include <dirent.h>
+    #include <sys/wait.h>
+    #include <signal.h>
+    #include <sys/sysmacros.h>
+    #include <sys/ioctl.h>
+    #include <termios.h>
+    #include <sys/select.h>
+    #include <sys/time.h>
+    #include <sys/utsname.h>
+    #include <sys/resource.h>
+    #include <sys/mman.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <netdb.h>
+    #include <poll.h>
+    #include <sys/epoll.h>
+    #include <sys/un.h>
+    #include <sys/ipc.h>
+    #include <sys/shm.h>
+    #include <sys/msg.h>
+    #include <sys/sem.h>
+    #include <pthread.h>
+    #include <semaphore.h>
+    #include <sys/prctl.h>
+    #include <sys/capability.h>
+    #include <sys/syscall.h>
+    #include <sys/ptrace.h>
+    #include <sys/user.h>
+    #include <sys/utsname.h>
+    #include <sys/sysinfo.h>
+    #include <sys/vfs.h>
+    #include <sys/times.h>
+    #include <sys/timex.h>
+    #include <sys/utsname.h>
+#endif //B_CPE_110_LIL_1_1_SECURED_ALEXANDRE_GARBE_INCLUDE_H
