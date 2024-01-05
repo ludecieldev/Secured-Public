6081711b2d3e01d6ead53e5a53bf3dc4b77c3010
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-01-05 20:07:21 +0100

    [+] Added secured.h

---
+++ include/secured.h
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPE-110-LIL-1-1-secured-alexandre.garbe
+** File description:
+** secured.h
+** Author:
+** ludeciel
+*/
+
+#ifndef B_CPE_110_LIL_1_1_SECURED_ALEXANDRE_GARBE_SECURED_H
+    #define B_CPE_110_LIL_1_1_SECURED_ALEXANDRE_GARBE_SECURED_H
+    #include "include.h"
+    #include "hashtable.h"
+
+int mini_printf(char const *format, ...);
+int my_getnbr(char *str);
+int my_put_nbr(int nb);
+int my_putstr(char const *str);
+void my_putchar(char c);
+int my_strlen(char const *str);
+int my_strcmp(char const *s1, char const *s2);
+
+#endif //B_CPE_110_LIL_1_1_SECURED_ALEXANDRE_GARBE_SECURED_H
