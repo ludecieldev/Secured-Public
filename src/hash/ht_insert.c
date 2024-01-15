86538a6daa3b8f0fea7818091b1b79364bc8a23e
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-01-15 18:59:57 +0100

    [+] Updated ht_insert.c

--- src/hash/ht_insert.c
+++ src/hash/ht_insert.c
@@ -11,7 +11,7 @@
 
 void insert_at_index(hasharray_t **array, hasharray_t *item, int index)
 {
-    items->next = array[index];
+    item->next = array[index];
     array[index] = item;
 }
 
@@ -20,13 +20,16 @@ int ht_insert(hashtable_t *ht, char *key, char *value)
     hasharray_t *item = malloc(sizeof(hasharray_t));
     int index = 0;
 
-    if (item == NULL)
+    if (item == NULL) {
+        free(item);
         return 84;
+    }
     item->key = my_strdup(key);
     item->hashkey = ht->hash(key, ht->len);
     item->value = my_strdup(value);
     item->next = NULL;
     index = item->hashkey % ht->len;
     insert_at_index(ht->array, item, index);
+    free(item);
     return (0);
 }
