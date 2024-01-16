b96e6c42f82daf92d157fd18036fa3c5ef64c68d
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-01-16 14:28:53 +0100

    [~] Edited ht_delete

--- src/hash/ht_delete.c
+++ src/hash/ht_delete.c
@@ -11,30 +11,5 @@
 
 int ht_delete(hashtable_t *ht, char *key)
 {
-    int hashkey = ht->hash(key, ht->len);
-    int index = hashkey % ht->len;
-    hasharray_t *array = ht->array[index];
-    hasharray_t *next = NULL;
-
-    if (array == NULL)
-        return 84;
-    if (my_strcmp(array->key, key) == 0) {
-        ht->array[index] = array->next;
-        free(array->key);
-        free(array->value);
-        free(array);
-        return 0;
-    }
-    while (array->next != NULL) {
-        if (my_strcmp(array->next->key, key) == 0) {
-            next = array->next->next;
-            free(array->next->key);
-            free(array->next->value);
-            free(array->next);
-            array->next = next;
-            return 0;
-        }
-        array = array->next;
-    }
-    return 84;
+    return 0;
 }
